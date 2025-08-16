fb842353e57e8206381da8c4d9693cbc4be6d81d
Koki Matsumoto <85651386+rokuosan@users.noreply.github.com>
2025-08-17 04:03:02 +0900

    実装の位置を変更する

---
+++ functions/bloom-socials/src/api/feed.ts
@@ -0,0 +1,70 @@
+import { Hono } from "hono";
+import { nanoid } from "nanoid";
+
+const app = new Hono<{ Bindings: CloudflareBindings }>();
+
+app.get("/feed", async (c) => {
+  const cursor = await c.req.query("cursor");
+
+  const images = await c.env.bloom_socials.list({
+    limit: 100,
+    cursor: cursor,
+  });
+
+  return c.json({
+    images: images,
+  });
+});
+
+app.post("/feed", async (c) => {
+  try {
+    const formData = await c.req.formData();
+    const file = formData.get("image");
+
+    if (!file) {
+      return c.json({ error: "Missing image" }, 400);
+    }
+    if (!(file instanceof File)) {
+      return c.json({ error: "Invalid image" }, 400);
+    }
+
+    const fileExtension = file.name.split(".").pop() || "jpg";
+    const newFileName = `${nanoid()}.${fileExtension}`;
+
+    await c.env.bloom_socials.put(newFileName, file.stream(), {
+      httpMetadata: {
+        contentType: file.type,
+      },
+    });
+
+    return c.json({ id: newFileName });
+  } catch (error) {
+    console.error("Error uploading image:", error);
+    return c.json({ error: "Failed to upload image" }, 500);
+  }
+});
+
+app.get("/feed/:id", async (c) => {
+  const { id } = c.req.param();
+
+  const image = await c.env.bloom_socials.get(id);
+
+  if (!image) {
+    return c.notFound();
+  }
+
+  return c.body(image.body, 200, {
+    "Content-Type": "image/jpeg",
+    "Cache-Control": "public, max-age=31536000, immutable",
+  });
+});
+
+app.delete("/feed/:id", async (c) => {
+  const { id } = c.req.param();
+
+  await c.env.bloom_socials.delete(id);
+
+  return c.json({ success: true });
+});
+
+export const v1Route = app;
