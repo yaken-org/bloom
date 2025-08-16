4ce5713e2e980891d803a8047834a15da6f9488c
ikotome <kotomizu.0728@gmail.com>
2025-08-17 05:31:29 +0900

    Dazzlingだけ一旦

--- components/filters/DazzlingFilter.tsx
+++ components/filters/DazzlingFilter.tsx
@@ -4,23 +4,38 @@ import type { FilterComponentProps } from "@/types/filters";
 
 const DazzlingFilter: React.FC<FilterComponentProps> = React.memo(
   ({ image, width, height, isBaseLayer = true, options = {} }) => {
-    const { opacity = 0.8, intensity = 1.0 } = options;
+    const { opacity = 0.9, intensity = 1.0 } = options;
 
-    const matrix = useMemo(() => {
+    const dazzlingMatrix = useMemo(() => {
       const i = intensity;
       return [
-        1.5*i, 0, 0, 0, 0,
-        0, 1.5*i, 0, 0, 0,
-        0, 0, 1.5*i, 0, 0,
+        1.8 * i, 0, 0, 0, 0,
+        0, 1.8 * i, 0, 0, 0,
+        0, 0, 1.8 * i, 0, 0,
+        0, 0, 0, 1, 0,
+      ];
+    }, [intensity]);
+
+    const contrastMatrix = useMemo(() => {
+      const i = intensity;
+      return [
+        2.5 * i, 0, 0, 0, -0.2,
+        0, 2.5 * i, 0, 0, -0.2,
+        0, 0, 2.5 * i, 0, -0.2,
         0, 0, 0, 1, 0,
       ];
     }, [intensity]);
 
     return (
       <Group>
-        {isBaseLayer && <Image image={image} x={0} y={0} width={width} height={height} fit="cover" />}
-        <Image image={image} x={0} y={0} width={width} height={height} fit="cover" opacity={opacity}>
-          <ColorMatrix matrix={matrix} />
+        {isBaseLayer && (
+          <Image image={image} x={0} y={0} width={width} height={height} fit="cover" />
+        )}
+        <Image image={image} x={0} y={0} width={width} height={height} fit="cover" opacity={opacity} blendMode="screen">
+          <ColorMatrix matrix={dazzlingMatrix} />
+        </Image>
+        <Image image={image} x={0} y={0} width={width} height={height} fit="cover" opacity={opacity * 0.7} blendMode="overlay">
+          <ColorMatrix matrix={contrastMatrix} />
         </Image>
       </Group>
     );
