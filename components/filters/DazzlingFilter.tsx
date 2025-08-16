d8be8bfc17b7d58ba7e0ad8c168ec5846d15e764
ikotome <kotomizu.0728@gmail.com>
2025-08-17 05:45:17 +0900

    輝きと色味

--- components/filters/DazzlingFilter.tsx
+++ components/filters/DazzlingFilter.tsx
@@ -2,16 +2,20 @@ import { ColorMatrix, Group, Image } from "@shopify/react-native-skia";
 import React, { useMemo } from "react";
 import type { FilterComponentProps } from "@/types/filters";
 
+/**
+ * DazzlingFilter
+ * ゴールド・白系の眩しい光を中心に派手ギラギラ
+ */
 const DazzlingFilter: React.FC<FilterComponentProps> = React.memo(
   ({ image, width, height, isBaseLayer = true, options = {} }) => {
     const { opacity = 0.9, intensity = 1.0 } = options;
 
-    const dazzlingMatrix = useMemo(() => {
+    const goldMatrix = useMemo(() => {
       const i = intensity;
       return [
-        1.8 * i, 0, 0, 0, 0,
-        0, 1.8 * i, 0, 0, 0,
-        0, 0, 1.8 * i, 0, 0,
+        1.3 * i + (1 - i), 0.2 * i, 0, 0, 0.05 * i,
+        0.1 * i, 1.2 * i + (1 - i), 0.1 * i, 0, 0.05 * i,
+        0, 0.1 * i, 1.4 * i + (1 - i), 0, 0.05 * i,
         0, 0, 0, 1, 0,
       ];
     }, [intensity]);
@@ -19,9 +23,9 @@ const DazzlingFilter: React.FC<FilterComponentProps> = React.memo(
     const contrastMatrix = useMemo(() => {
       const i = intensity;
       return [
-        2.5 * i, 0, 0, 0, -0.2,
-        0, 2.5 * i, 0, 0, -0.2,
-        0, 0, 2.5 * i, 0, -0.2,
+        1.5 * i + (1 - i), 0, 0, 0, -0.05 * i,
+        0, 1.4 * i + (1 - i), 0, 0, -0.05 * i,
+        0, 0, 1.6 * i + (1 - i), 0, -0.05 * i,
         0, 0, 0, 1, 0,
       ];
     }, [intensity]);
@@ -31,10 +35,21 @@ const DazzlingFilter: React.FC<FilterComponentProps> = React.memo(
         {isBaseLayer && (
           <Image image={image} x={0} y={0} width={width} height={height} fit="cover" />
         )}
-        <Image image={image} x={0} y={0} width={width} height={height} fit="cover" opacity={opacity} blendMode="screen">
-          <ColorMatrix matrix={dazzlingMatrix} />
+
+        <Image image={image} x={0} y={0} width={width} height={height} fit="cover" opacity={opacity}>
+          <ColorMatrix matrix={goldMatrix} />
         </Image>
-        <Image image={image} x={0} y={0} width={width} height={height} fit="cover" opacity={opacity * 0.7} blendMode="overlay">
+
+        <Image
+          image={image}
+          x={0}
+          y={0}
+          width={width}
+          height={height}
+          fit="cover"
+          opacity={opacity * 0.7}
+          blendMode="screen"
+        >
           <ColorMatrix matrix={contrastMatrix} />
         </Image>
       </Group>
--- components/filters/RainbowFilter.tsx
+++ components/filters/RainbowFilter.tsx
@@ -2,16 +2,20 @@ import { ColorMatrix, Group, Image } from "@shopify/react-native-skia";
 import React, { useMemo } from "react";
 import type { FilterComponentProps } from "@/types/filters";
 
+/**
+ * RainbowFilter
+ * 虹色グラデーション＋発光感で超カラフル
+ */
 const RainbowFilter: React.FC<FilterComponentProps> = React.memo(
   ({ image, width, height, isBaseLayer = true, options = {} }) => {
-    const { opacity = 0.9, intensity = 1.0 } = options;
+    const { opacity = 0.8, intensity = 1.0 } = options;
 
     const rainbowMatrix = useMemo(() => {
       const i = intensity;
       return [
-        1.5 * i, 0, 0.5 * i, 0, 0,
-        0.5 * i, 1.5 * i, 0, 0, 0,
-        0, 0.5 * i, 1.5 * i, 0, 0,
+        1.5*i, 0.0, 0.2*i, 0, 0.0,
+        0.0, 1.5*i, 0.2*i, 0, 0.0,
+        0.2*i, 0.2*i, 1.5*i, 0, 0.0,
         0, 0, 0, 1, 0,
       ];
     }, [intensity]);
@@ -19,10 +23,10 @@ const RainbowFilter: React.FC<FilterComponentProps> = React.memo(
     const glowMatrix = useMemo(() => {
       const i = intensity;
       return [
-        2.0 * i, 0, 0, 0, 0,
-        0, 2.0 * i, 0, 0, 0,
-        0, 0, 2.0 * i, 0, 0,
-        0, 0, 0, 1, 0,
+        1.3*i, 0.1*i, 0.1*i, 0, 0,
+        0.1*i, 1.3*i, 0.1*i, 0, 0,
+        0.1*i, 0.1*i, 1.3*i, 0, 0,
+        0,0,0,1,0,
       ];
     }, [intensity]);
 
@@ -31,10 +35,21 @@ const RainbowFilter: React.FC<FilterComponentProps> = React.memo(
         {isBaseLayer && (
           <Image image={image} x={0} y={0} width={width} height={height} fit="cover" />
         )}
-        <Image image={image} x={0} y={0} width={width} height={height} fit="cover" opacity={opacity} blendMode="screen">
+
+        <Image image={image} x={0} y={0} width={width} height={height} fit="cover" opacity={opacity}>
           <ColorMatrix matrix={rainbowMatrix} />
         </Image>
-        <Image image={image} x={0} y={0} width={width} height={height} fit="cover" opacity={opacity * 0.8} blendMode="overlay">
+
+        <Image
+          image={image}
+          x={0}
+          y={0}
+          width={width}
+          height={height}
+          fit="cover"
+          opacity={opacity*0.6}
+          blendMode="screen"
+        >
           <ColorMatrix matrix={glowMatrix} />
         </Image>
       </Group>
