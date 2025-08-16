4553dc5967238b789c2ca4ea95d9ff284d454ae0
taiseiue <taiseiue@wsnet.jp>
2025-08-16 22:56:18 +0900

    勝った

--- hooks/useFilters.ts
+++ hooks/useFilters.ts
@@ -91,8 +91,6 @@ export const useFilters = (
       order: manager.getFilterOrder(),
       options: manager.getAllFilterOptions(),
     }),
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    // biome-ignore lint/correctness/useExhaustiveDependencies: updateTriggerは再レンダリングトリガーとして使っている
     [manager, updateTrigger],
   );
 
