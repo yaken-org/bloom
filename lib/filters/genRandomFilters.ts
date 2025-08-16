fe6b1e7ee4ce83fe046b9491b4bc0c168e5d03a5
taiseiue <taiseiue@wsnet.jp>
2025-08-17 02:16:16 +0900

    ランダムにフィルターを選択するusecase書いた

---
+++ lib/filters/genRandomFilters.ts
@@ -0,0 +1,19 @@
+
+import { filterFactory } from "@/lib/filters/FilterFactory";
+/**
+ * 現在登録されているフィルターから2つをランダムに選ぶユースケース
+ * @returns {string[]} ランダムに選択された2つのフィルター名の配列
+ */
+export default function getRandomFilters(): string[] {
+    // 登録されている全フィルターのタイプを取得
+    const availableFilters = filterFactory.getAvailableFilterTypes();
+
+    // フィルターが2つ未満の場合は利用可能な全てを返す
+    if (availableFilters.length <= 2) {
+        return availableFilters;
+    }
+
+    // ランダムに2つ選択
+    const shuffled = [...availableFilters].sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, 2);
+};
