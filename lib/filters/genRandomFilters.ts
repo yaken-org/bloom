59b5deb8a141655692a5a415c5396a134dc7599e
taiseiue <taiseiue@wsnet.jp>
2025-08-17 09:21:42 +0900

    とりあえずフレームの数を増やした

--- lib/filters/genRandomFilters.ts
+++ lib/filters/genRandomFilters.ts
@@ -10,12 +10,12 @@ const FRAME_IMAGES = [
   //require("@/assets/flames/red_frame.png"),
   //require("@/assets/flames/yellow_frame.png"),
   //require("@/assets/flames/wabi_orange.png"),
-  //   require("@/assets/flames/gira_green.png"),
-  //   require("@/assets/flames/gira_purple.png"),
-  //   require("@/assets/flames/gira_blue.png"),
-  //   require("@/assets/flames/gira_red.png"),
-  //   require("@/assets/flames/gira_yellow.png"),
-  //   require("@/assets/flames/mirror_orange.png"),
+  require("@/assets/flames/gira_green.png"),
+  require("@/assets/flames/gira_purple.png"),
+  require("@/assets/flames/gira_blue.png"),
+  require("@/assets/flames/gira_red.png"),
+  require("@/assets/flames/gira_yellow.png"),
+  require("@/assets/flames/mirror_orange.png"),
   require("@/assets/flames/gira_photo.png"),
 ];
 
